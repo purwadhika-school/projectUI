898fa4b8c01c8b47cc35ffeecc228f6c61dc88d5
yogie.putra8 <yogie.putra8@gmail.com>
2018-04-10 21:12:53 +0700

    add register

--- FinPlan/src/Setup.js
+++ FinPlan/src/Setup.js
@@ -9,10 +9,11 @@ import { StyleSheet, ActivityIndicator, Text, View } from "react-native";
 import { connect } from "react-redux";
 import { fetchData } from "./actions";
 import Login from './screens/login'
+import Register from './screens/register'
 
 class Setup extends Component {
   render() {
-    return <Login />
+    return <Register />
   }
 }
 
